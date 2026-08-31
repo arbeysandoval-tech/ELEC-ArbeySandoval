cf27a51ac8f45bc1d3318a014b594be2688275c8
arbeysandoval-tech <arbey.sandoval@uptc.edu.co>
2026-08-31 18:15:26 -0500

    Add files via upload

---
+++ 01_T.DATOS.py
@@ -0,0 +1,37 @@
+#ctrl k+c convertir en comentario
+#ctrl k+u quitar comentario
+#CLS Borrar historial terminal
+# print(20)
+# print(4.5)
+# print("hola mundo")
+# print(True)
+
+
+
+# #variable
+# nombre ="Vladimir"
+# edad = (36)
+# Estatura = (1.70)
+# soy_alto = False
+# semestre = 9 #nunca lleva numero al incio de el codigo
+# print(nombre,edad,Estatura,soy_alto,semestre)
+
+
+
+#tipo de dato
+# print(type(nombre))
+# print(type(edad))
+# print(type(Estatura))
+# print(type(soy_alto))
+
+edad = 20
+nombre = "Arbey"
+estatura = 1.79
+semestre = 8
+edae_en_5_anos = edad+5
+
+#calculadora
+valor1 = int(input("ingrese un valor"))
+valor2 = int(input("ingrese un valor"))
+resultado = valor1+valor2
+print("el resultado:",resultado)
