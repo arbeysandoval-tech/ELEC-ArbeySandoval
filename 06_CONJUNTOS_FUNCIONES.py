5ca531c2df1bda94cba127f689b55d6d656a4384
arbeysandoval-tech <arbey.sandoval@uptc.edu.co>
2026-09-14 19:49:48 -0500

    Add files via upload

---
+++ 06_CONJUNTOS_FUNCIONES.py
@@ -0,0 +1,38 @@
+# # estudiantes=
+
+# # cantidad_estudiantes=int (input("Numero de estudiantes: "
+
+# # for i in range(cantidad_estudiantes
+
+# #   nombre = input ("nombre estudiante
+
+# #   nota1 = float(input("digite su nota"
+
+# #   nota2 =float(input("digite su nota"
+
+# #   nota3 =float( input("digite su nota"
+
+# #   notas =[nota1,nota2,nota
+
+# #   estudiante =
+# #    "nombre": nombr
+# #     "notas" : not
+
+# #  
+# #   estudiantes.append(estudiant
+# #   for i in( estudiantes
+# #     promedio = sum(notas) / len(nota
+# #     print (estudiante["nombre"], round (promedio,2
+
+numeros={10,20,30,30,40}
+nomb = {10,25,30,45}
+union =((numeros)|(nomb))
+datos=set()
+if 10 in numeros:
+	numeros.add(50)  
+	numeros.remove(30)
+	numeros.discard(60)
+print(numeros|nomb)
+
+
+
